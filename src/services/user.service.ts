2875bf74ee4a4082b9c91effdb2cf2a66d9102f2
Xavier Abad <77491413+masterprog-cmd@users.noreply.github.com>
2024-04-26 08:56:22 +0200

    fix: move the API url to .env

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,7 @@
+const IP_API_URL = import.meta.env.VITE_IP_API_URL
+
 export const getUserIp = async () => {
-  const request = await fetch('https://ipinfo.io/json')
+  const request = await fetch(`${IP_API_URL}/json`)
   const data = await request.json()
 
   const { ip, city, region, country } = data
