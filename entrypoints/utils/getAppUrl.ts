a10b7d001cd070010b8b3402ab6b2656963f5bd4
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-02-14 12:12:37 +0100

    feat: use env variable

--- entrypoints/utils/getAppUrl.ts
+++ entrypoints/utils/getAppUrl.ts
@@ -3,7 +3,7 @@ type EnvironmentMode = 'development' | 'staging' | 'production'
 const APP_URLS: Record<EnvironmentMode, string> = {
   development: 'http://localhost:3000',
   staging: 'https://staging.drive.internxt.com',
-  production: 'https://drive.internxt.com',
+  production: import.meta.env.VITE_AUTH_HOST_URL,
 }
 
 export function getAppUrl(mode: string): string {
